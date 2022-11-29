460f51cd2d3df7c4a878da9fbd10f0f17d13d0c5
Leograzioli <leograzioli@gmail.com>
2022-11-29 13:41:04 +0100

    createapp setted

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Todo List</title>
 
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -17,7 +17,10 @@
 
 <body>
 
-<script src="js/script.js"></script>
+   <div id="app">
+   </div>
+
+    <script src="js/script.js"></script>
 </body>
 
 </html>
