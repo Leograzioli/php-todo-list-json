366ebc96c1cea5168bb5301845e6a1097b0a0fc0
Leograzioli <leograzioli@gmail.com>
2022-11-30 10:44:20 +0100

    add elseif instead of if

--- server.php
+++ server.php
@@ -3,18 +3,17 @@ $todo = file_get_contents('todo.json');
 $todo_list =json_decode($todo, true);
 
 if(isset($_POST["text"])) {
+
     $input_txt = $_POST["text"];
     $todo_list[] = ["text" => $input_txt, "done" => false ];
     file_put_contents("todo.json", json_encode($todo_list));
-}
+} elseif (isset($_POST["i"])) {
 
-if (isset($_POST["i"])) {
     $done = $_POST["i"];
     $todo_list[$done]["done"] = !$todo_list[$done]["done"];
     file_put_contents("todo.json", json_encode($todo_list));
-}
-
-if (isset($_POST["index"])) {
+} elseif (isset($_POST["index"])) {
+    
     $index = $_POST['index'];
     array_splice($todo_list, $index, 1);
     file_put_contents("todo.json", json_encode($todo_list));
